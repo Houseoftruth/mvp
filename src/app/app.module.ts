dd3ced6138646a2942d9daa26c17c2db3eea53b4
ohrha <trigramthree@gmail.com>
2019-12-30 17:01:57 -0800

    'CREATE-COMPONENTS-FOR-CUSTOMER-AND-KITCHEN'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // 
+import { RouterModule, Routes } from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KitchenComponent } from './kitchen/kitchen.component';
+import { CustomerComponent } from './customer/customer.component';
+const appRoutes: Routes = [
+  { path: 'kitchen', component: KitchenComponent },
+  { path: 'customer', component: CustomerComponent },
+  { path: '',
+  redirectTo: '/customer',
+  pathMatch: 'full'
+},
 
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KitchenComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
